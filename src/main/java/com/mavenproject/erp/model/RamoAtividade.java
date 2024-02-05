ba7f78a7cb9fca1c015951fed6b1c71a73ca50f0
Walber Costa <walberoc@gmail.com>
2024-02-05 06:38:29 -0300

    update ok - entidades Empresa, RamoAtividade, TipoEmpresa

---
+++ src/main/java/com/mavenproject/erp/model/RamoAtividade.java
@@ -0,0 +1,67 @@
+package com.mavenproject.erp.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ramo_atividade")
+public class RamoAtividade implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false,length = 80)
+    private String descricao;//semelhante ao varchar no DB
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RamoAtividade other = (RamoAtividade) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "RamoAtividade{" + "id=" + id + '}';
+    }
+    
+
+}
