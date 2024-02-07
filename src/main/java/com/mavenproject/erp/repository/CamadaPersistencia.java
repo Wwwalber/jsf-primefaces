bd6dad8cf86f2921971a0be3981349da4e8ac3d4
Walber Costa <walberoc@gmail.com>
2024-02-07 19:22:20 -0300

    update ignore

---
+++ src/main/java/com/mavenproject/erp/repository/CamadaPersistencia.java
@@ -0,0 +1,59 @@
+package com.mavenproject.erp.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.mavenproject.erp.model.Empresa;
+import com.mavenproject.erp.model.RamoAtividade;
+import com.mavenproject.erp.model.TipoEmpresa;
+
+/**
+ *
+ * @author walber
+ */
+public class CamadaPersistencia {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("AlgaWorksPU");
+
+        EntityManager em = emf.createEntityManager();
+    
+        em.getTransaction().begin();// se não iniciar a transção pode fazer somente consultas
+
+        // declarando os repositórios
+        RamoAtividades ramoAtividades = new RamoAtividades(em);
+        Empresas empresas = new Empresas(em);
+
+        // buscando as informações no banco
+        List<RamoAtividade> listaDeRamoAtividades = ramoAtividades.pesquisar("");// traz todos os registros
+        List<Empresa> listaDeEmpresas = empresas.pesquisar(""); // traz todos os registros
+        System.out.println(listaDeEmpresas);
+ 
+        // criando uma empresa
+        
+        Empresa empresa = new Empresa();
+        empresa.setNomeFantasia("João da Silva");
+        empresa.setCnpj("41.952.519/0001-57");
+        empresa.setRazaoSocial("João da Silva 41952519000157");
+        empresa.setTipo(TipoEmpresa.MEI);
+        empresa.setDataFundacao(new Date());
+        empresa.setRamoAtividade(listaDeRamoAtividades.get(0));// pegando o primeiro registro
+
+        // salvando a empresa
+        empresas.guardar(empresa);
+
+        em.getTransaction().commit(); //
+
+        //verificar se a transação funcionou
+        List<Empresa> listaDeEmpresas2 = empresas.pesquisar("");
+        System.out.println(listaDeEmpresas2);
+
+        em.close();
+        emf.close();
+    }
+}
+
--- src/main/java/com/mavenproject/erp/repository/RamoAtividades.java
+++ src/main/java/com/mavenproject/erp/repository/RamoAtividades.java
@@ -1,11 +1,45 @@
 package com.mavenproject.erp.repository;
-
+import com.mavenproject.erp.model.RamoAtividade;
 import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 /**
  *
  * @author walber
  */
-public class RamoAtividades implements Serializable {
+public class RamoAtividades implements Serializable{
+    private static final Long serialVersionUID = 1L;
     
+    private EntityManager manager;
+
+    public RamoAtividades(){
+    }
+
+    public RamoAtividades(EntityManager manager){
+        this.manager = manager;
+    }
+    
+    // implementar utilizando criteria
+    public List<RamoAtividade> pesquisar(String descricao){
+        CriteriaBuilder criteriaBuilder = manager.getCriteriaBuilder();
+
+        // aqui é mais complexo do que usar jpql. Pode-se fazer das duas maneiras
+        CriteriaQuery<RamoAtividade> criteriaQuery = criteriaBuilder.createQuery(RamoAtividade.class);
+        // epécie de alias
+        /*  por exemplo
+         *      select ra.* from ramo_atividade ra;
+         */
+        Root<RamoAtividade> root = criteriaQuery.from(RamoAtividade.class);
+        criteriaQuery.select(root);
+        criteriaQuery.where(criteriaBuilder.like(root.get("descricao"), descricao +"%"));
+
+        TypedQuery<RamoAtividade> query = manager.createQuery(criteriaQuery);
+        return query.getResultList();
+    }
 }
