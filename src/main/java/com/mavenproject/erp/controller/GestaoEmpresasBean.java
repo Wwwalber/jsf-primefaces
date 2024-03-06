630594dd5bb8838ea3ae0eeb111e344d97e43ea4
Walber Costa <walberoc@gmail.com>
2024-03-06 13:43:33 -0300

    testando o escopo de Seção

--- src/main/java/com/mavenproject/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/com/mavenproject/erp/controller/GestaoEmpresasBean.java
@@ -1,11 +1,12 @@
 package com.mavenproject.erp.controller;
 
 import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
 @Named
-@ViewScoped // se inicia quando o usuário entrar na página. Ou seja, mesmo enviando chamadas post não há mais iteração
+@SessionScoped// se inicia quando é criada a seção
 public class GestaoEmpresasBean implements Serializable{
     
     private static final long serialVersionUID = 1L;
